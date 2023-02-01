f6c8b78baa756beff435d036d763f1219b9ac905
MarcoAS99 <antonioo.dsm@gmail.com>
2023-02-01 12:00:33 +0100

    Initial commit

---
+++ src/main/java/com/marcoas99/youtubescraper/YoutubeScraperApplication.java
@@ -0,0 +1,13 @@
+package com.marcoas99.youtubescraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeScraperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcoas99/youtubescraper/YoutubeScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoas99.youtubescraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
