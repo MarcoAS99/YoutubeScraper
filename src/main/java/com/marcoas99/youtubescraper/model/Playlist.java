3807d0644f81041de9fd92eff6987f7893d8faf6
MarcoAS99 <antonioo.dsm@gmail.com>
2023-02-10 15:02:32 +0100

    Changed models to entities.

--- src/main/java/com/marcoas99/youtubescraper/model/Playlist.java
+++ src/main/java/com/marcoas99/youtubescraper/model/Playlist.java
@@ -1,13 +1,20 @@
 package com.marcoas99.youtubescraper.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Data
 @Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Playlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
+    @OneToMany(targetEntity = Video.class)
     private List<Video> videoList;
 }
--- src/main/java/com/marcoas99/youtubescraper/model/Video.java
+++ src/main/java/com/marcoas99/youtubescraper/model/Video.java
@@ -1,19 +1,24 @@
 package com.marcoas99.youtubescraper.model;
 
-import lombok.Builder;
-import lombok.Data;
-
+import lombok.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.net.URL;
-import java.time.Duration;
 
 @Data
 @Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     private URL url;
     private String duration;
     private String views;
-    private String when;
-
-
+    private String uploaded;
 }
