e9cb0b7ead5f2adf84949b5dc43120857b941a97
MarcoAS99 <antonioo.dsm@gmail.com>
2023-02-10 15:03:04 +0100

    Added H2 db and JPA.

--- src/main/java/com/marcoas99/youtubescraper/api/HomePageController.java
+++ src/main/java/com/marcoas99/youtubescraper/api/HomePageController.java
@@ -1,12 +1,10 @@
 package com.marcoas99.youtubescraper.api;
 
-import com.marcoas99.youtubescraper.model.HomePage;
+import com.marcoas99.youtubescraper.model.Channel;
 import com.marcoas99.youtubescraper.service.HomePageService;
-import com.marcoas99.youtubescraper.service.YoutubeScraperService;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,12 +21,10 @@ public class HomePageController {
     private final HomePageService homePageService;
 
     @PostMapping("/homepage")
-    @ApiOperation(value = "Given a YouTube HomePage URL it returns the a resume of the HomePage.",
-    consumes = MediaType.TEXT_PLAIN_VALUE,
-    produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<HomePage> getHomePage(@RequestBody String homePageUrlString) throws MalformedURLException{
-        HomePage homePage = homePageService.getHomePage(homePageUrlString);
-        return ResponseEntity.ok(homePage);
+    @Operation(description = "Given a YouTube Channel URL it returns the a resume of the HomePage.")
+    public ResponseEntity<Channel> getChannel(@RequestBody String channelUrlString) throws MalformedURLException{
+        Channel channel = homePageService.getHomePage(channelUrlString);
+        return ResponseEntity.ok(channel);
     }
 
 }
--- src/main/java/com/marcoas99/youtubescraper/service/HomePageService.java
+++ src/main/java/com/marcoas99/youtubescraper/service/HomePageService.java
@@ -1,8 +1,12 @@
 package com.marcoas99.youtubescraper.service;
 
-import com.marcoas99.youtubescraper.model.HomePage;
+import com.marcoas99.youtubescraper.model.Channel;
 import com.marcoas99.youtubescraper.model.Playlist;
 import com.marcoas99.youtubescraper.model.Video;
+import com.marcoas99.youtubescraper.repository.ChannelRepository;
+import com.marcoas99.youtubescraper.repository.PlaylistRepository;
+import com.marcoas99.youtubescraper.repository.VideoRepository;
+import lombok.RequiredArgsConstructor;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
 
@@ -11,27 +15,33 @@ import java.net.URL;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class HomePageService {
 
     private final YoutubeScraperService youtubeScraperService;
+    private final VideoRepository videoRepository;
+    private final PlaylistRepository playlistRepository;
+    private final ChannelRepository channelRepository;
 
-    public HomePageService(YoutubeScraperService youtubeScraperService) {
-        this.youtubeScraperService = youtubeScraperService;
-    }
-
-    public HomePage getHomePage(String homePageUrl) throws MalformedURLException {
+    public Channel getHomePage(String homePageUrl) throws MalformedURLException {
         Document document = youtubeScraperService.getPage(homePageUrl);
         Video featuredVideo = youtubeScraperService.getFeaturedVideo(document);
         String channelName = youtubeScraperService.getChannelName(document);
         String subscriberCount = youtubeScraperService.getChannelSubscribers(document);
         List<Playlist> playlistList = youtubeScraperService.getPlaylistList(document);
 
-        return HomePage.builder()
-                .channelName(channelName)
-                .channelUrl(new URL(homePageUrl))
+        videoRepository.save(featuredVideo);
+        playlistList.forEach(playlist-> videoRepository.saveAll(playlist.getVideoList()));
+        playlistRepository.saveAll(playlistList);
+
+        Channel channel = Channel.builder()
+                .name(channelName)
+                .url(new URL(homePageUrl))
                 .subscriberCount(subscriberCount)
                 .featuredVideo(featuredVideo)
                 .featuredPlaylists(playlistList)
                 .build();
+
+        return channelRepository.save(channel);
     }
 }
