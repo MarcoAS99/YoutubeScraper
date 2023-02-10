2666ea46d50021ccddbf1e29bf5906ca9a9ba7b6
MarcoAS99 <antonioo.dsm@gmail.com>
2023-02-10 15:01:22 +0100

    Added JPA Repositories.

---
+++ src/main/java/com/marcoas99/youtubescraper/repository/ChannelRepository.java
@@ -0,0 +1,7 @@
+package com.marcoas99.youtubescraper.repository;
+
+import com.marcoas99.youtubescraper.model.Channel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChannelRepository extends JpaRepository<Channel, Long> {
+}
